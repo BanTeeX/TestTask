b46bec17ce305cbf988850e9e25c47beff719936
Filip Banaszak <filipbanaszak333@gmail.com>
2021-06-13 01:14:27 +0200

    Created Actor prefab
    I made scrpit defines Actors behaviour and made simple gameobject representing it.

---
+++ Assets/Actor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Actor : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed;
+	[SerializeField]
+	private int _life;
+	private Vector2 _direction;
+	private Rigidbody2D _rigidbody;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_direction = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward) * Vector2.up;
+		_rigidbody.velocity = _direction * _speed;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		_direction = Vector2.Reflect(_direction, collision.GetContact(0).normal);
+		_rigidbody.velocity = _direction * _speed;
+		if (collision.collider.CompareTag("Actor"))
+		{
+			_life--;
+		}
+		if (_life == 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
